e398a95871ca58f5b40d108c0a6bb4661beba374
Lee <toxicnaan@gmail.com>
2014-10-14 03:13:37 +0100

    second lesson

--- rain/src/com/lee/rain/Game.java
+++ rain/src/com/lee/rain/Game.java
@@ -1,12 +1,26 @@
 package com.lee.rain;
 
-public class Game {
+public class Game implements Runnable {
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
+	private Thread thread;
 	
 	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
 
+	
+	public synchronized void stop()  {
+		try {
+		thread.join();
+		} catch (InterruptedException e)  {
+			e.printStackTrace();
+		}
+	}
+	
 }
