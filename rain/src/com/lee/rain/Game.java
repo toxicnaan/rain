1b057d5d453938741910dd594cdf4a6d3d5a9ecd
Lee <toxicnaan@gmail.com>
2014-10-14 04:15:48 +0100

    end of part 4

--- rain/src/com/lee/rain/Game.java
+++ rain/src/com/lee/rain/Game.java
@@ -1,26 +1,88 @@
+//pt 4
+
+
 package com.lee.rain;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;;
+
+
+public class Game extends Canvas implements Runnable {
+	
+	
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
 	
+	public Game()  {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+		
+	}
 	
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 
 	
 	public synchronized void stop()  {
+		running = false;
 		try {
 		thread.join();
 		} catch (InterruptedException e)  {
 			e.printStackTrace();
 		}
+	}
+
+	public void run() {
+		while (running)  {
+			//System.out.println("Running!...");
+			update();
+			render();
+			
+		}
+		
+	}
+	
+	public void update()  {
+		
+	}
+	
+	public void render() {
+		
+		
+		
+	}
+	
+	public static void main(String[] args)  {
+		
+		Game game = new Game();
+		
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
+		
+		
+		
+		
 	}
 	
 }
