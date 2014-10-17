a2747f4e1ce2b36eb0aa2f27fc08417dd6a59353
Lee <toxicnaan@gmail.com>
2014-10-17 12:20:38 +0100

    quick

--- rain/src/com/lee/rain/graphics/Screen.java
+++ rain/src/com/lee/rain/graphics/Screen.java
@@ -1,10 +1,18 @@
 package com.lee.rain.graphics;
 
+import java.util.Random;
+
 public class Screen {
 	
 	private int width, height;
 	
 	public int[] pixels;
+	
+	public int[] tiles = new int [64 *64];
+	
+	private Random random = new Random();
+	
+	
 	
 	public Screen(int width, int height) {
 		this.width = width;
@@ -12,11 +20,30 @@ public class Screen {
 		pixels = new int[width * height];
 		
 		
+		for (int i = 0; i < 64*64; i++) {
+				tiles[i] = random.nextInt(0xffffff);
+		}
+		
+	}
+	
+	
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
 	}
 	
 	public void render() {
+		
 		for (int y = 0; y < height; y++) {
+			
+			if (y < 0 || y >= height) break;			
+			
 			for (int x = 0 ; x < width; x++) {
+				
+				if (x <0 || x >= width) break;
+				
+				int tileIndex
 				pixels[x + y * width] = 0xFF00FF;
 				
 			}
